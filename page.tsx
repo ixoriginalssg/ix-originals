185a8fcd33c4910086c13cf2bb31ab8e1e48465c
ixoriginalssg <ixoriginalssg@gmail.com>
2026-07-03 15:06:33 +0800

    update website layout order

---
+++ page.tsx
@@ -0,0 +1,41 @@
+import Nav from '@/components/Nav'
+import Hero from '@/components/Hero'
+import About from '@/components/About'
+import Founder from '@/components/Founder'
+import Process from '@/components/Process'
+import Tiers from '@/components/Tiers'
+import Drops from '@/components/Drops'
+import Order from '@/components/Order'
+import Gallery from '@/components/Gallery'
+import FAQ from '@/components/FAQ'
+import Footer from '@/components/Footer'
+import ScrollEffects from '@/components/ScrollEffects'
+
+export default function Home() {
+  return (
+    <>
+      <ScrollEffects />
+      <Nav />
+      <main>
+        <Hero />
+        <div className="holoRule" />
+        <Gallery />
+        <div className="holoRule" />
+        <About />
+        <div className="holoRule" />
+        <Founder />
+        <div className="holoRule" />
+        <Process />
+        <div className="holoRule" />
+        <Tiers />
+        <div className="holoRule" />
+        <Drops />
+        <div className="holoRule" />
+        <Order />
+        <FAQ />
+        <div className="holoRule" />
+      </main>
+      <Footer />
+    </>
+  )
+}
